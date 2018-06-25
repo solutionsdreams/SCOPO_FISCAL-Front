70f504ec51bf1a0125c45f4c1a4b941a9117fea7
josiasmaluko <josias_analise_sistemas@hotmail.com>
2018-06-25 17:24:20 -0300

    Comit
    comit

---
+++ SCOPO_FISCAL/Scripts/sb-admin-datatables.js
@@ -0,0 +1,19 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+    $('#dataTable1').DataTable();
+    $('#dataTable2').DataTable();
+    $('#dataTable0021').DataTable();
+    $('#dataTable0022').DataTable();
+    $('#dataTable0023').DataTable();
+    $('#dataTable0024').DataTable();
+    $('#dataTable0025').DataTable();
+    $('#dataTable0025').DataTable();
+    $('#dataTable0026').DataTable();
+    $('#dataTable0027').DataTable();
+    $('#dataTable0028').DataTable();
+    $('#dataTable0029').DataTable();
+    $('#dataTable0030').DataTable();
+});
+
+
